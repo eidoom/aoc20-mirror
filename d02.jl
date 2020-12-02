723313ec4e33292ecc6345535cb53bc07729dc42
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-02 21:56:05 +0000

    Solve dat 2b

--- d02.jl
+++ d02.jl
@@ -30,3 +30,17 @@ end
 Test.@test valid(read("i02t0")) == 2
 
 @time println(valid(read("i02")))
+
+function valid2(data)
+    v = 0
+    for c in data
+        if xor(c["pas"][c["low"]] == c["ltr"], c["pas"][c["upp"]] == c["ltr"])
+            v += 1
+        end
+    end
+    v
+end
+
+Test.@test valid2(read("i02t0")) == 1
+
+@time println(valid2(read("i02")))
