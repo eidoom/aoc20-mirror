3d2d1dc2464c8170a6c7160a746e7b7e539af6c3
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-10 14:52:47 +0000

    Declared types in 10

--- d10.jl
+++ d10.jl
@@ -5,18 +5,17 @@ import Test
 include("./com.jl")
 include("./nacci.jl")
 
-function read_file(name)
+function read_file(name)::Array{Int,1}
     sort(map(x -> parse(Int, x), Com.file_lines(name)))
 end
 
-function first(data)
-    one = data[1] == 1 ? 1 : 0
-    three = 1 + (data[1] == 3 ? 1 : 0)
-    for i = 2:length(data)
-        j = data[i] - data[i - 1]
-        if j == 1
+function first(data::Array{Int,1})::Int
+    one::Int = data[1] === 1 ? 1 : 0
+    three::Int = 1 + (data[1] === 3 ? 1 : 0)
+    for i::Int = 2:length(data)
+        if data[i] - data[i - 1] === 1
             one += 1
-        elseif j == 3
+        else
             three += 1
         end
     end
@@ -27,21 +26,20 @@ t0 = read_file("i10t0")
 t1 = read_file("i10t1")
 inp = read_file("i10")
 
-Test.@test first(t0) == 35
-Test.@test first(t1) == 220
+Test.@test first(t0) === 35
+Test.@test first(t1) === 220
 
 @time a = first(inp)
 println(a)
-Test.@test a == 2312
+Test.@test a === 2312
 
-function second(data)
-    cache = Dict{Int, Int}(1=>1, 2=>2, 3=>4)
+function second(data::Array{Int,1})::Int
+    cache = Dict{Int,Int}(1 => 1, 2 => 2, 3 => 4)
     pushfirst!(data, 0)
-    count = 0
-    total = 1
-    for i = 2:length(data)
-        j = data[i] - data[i - 1]
-        if j == 1
+    count::Int = 0
+    total::Int = 1
+    for i::Int = 2:length(data)
+        if data[i] - data[i - 1] === 1
             count += 1
         else
             if count > 1
@@ -56,9 +54,9 @@ function second(data)
     total
 end
 
-Test.@test second(t0) == 8
-Test.@test second(t1) == 19208
+Test.@test second(t0) === 8
+Test.@test second(t1) === 19208
 
 @time b = second(inp)
 println(b)
-Test.@test b == 12089663946752
+Test.@test b === 12089663946752
