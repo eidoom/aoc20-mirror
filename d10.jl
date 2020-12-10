7a963695352ac6bf79315bac8ddcda0f369e789b
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-10 10:03:03 +0000

    Solve 10b

--- d10.jl
+++ d10.jl
@@ -5,11 +5,10 @@ import Test
 include("./com.jl")
 
 function read_file(name)
-    map(x -> parse(Int, x), Com.file_lines(name))
+    sort(map(x -> parse(Int, x), Com.file_lines(name)))
 end
 
 function first(data)
-    data = sort(data)
     one = data[1] == 1 ? 1 : 0
     three = 1 + (data[1] == 3 ? 1 : 0)
     for i = 2:length(data)
@@ -34,14 +33,60 @@ Test.@test first(t1) == 220
 println(a)
 Test.@test a == 2312
 
-function second(data)
-    data
+#= function fib(n, cache = Dict()) =#
+#=     if n <= 1 =#
+#=         n =#
+#=     else =#
+#=         for m in (n - 1, n - 2) =#
+#=             if !haskey(cache, m) =#
+#=                 cache[m] = fib(m, cache) =#
+#=             end =#
+#=         end =#
+#=         cache[n - 1] + cache[n - 2] =#
+#=     end =#
+#= end =#
+
+function trib(n, cache = Dict())
+    if n == 1
+        1
+    elseif n == 2
+        2
+    elseif n == 3
+        4
+    else
+        for m = (n - 3):(n - 1)
+            if !haskey(cache, m)
+                cache[m] = trib(m, cache)
+            end
+        end
+        cache[n - 1] + cache[n - 2] + cache[n - 3]
+    end
 end
 
-println(second(t0))
-#= Test.@test second(t0) == 0 =#
-#= Test.@test second(t1) == 0 =#
+function second(data)
+    pushfirst!(data, 0)
+    count = 0
+    total = 1
+    for i = 2:length(data)
+        j = data[i] - data[i - 1]
+        if j == 1
+            count += 1
+        else
+            if count > 1
+                total *= trib(count)
+            end
+            count = 0
+        end
+    end
+    if count > 1
+        total *= trib(count)
+    end
+    total
+end
 
-#= @time b = second(inp) =#
-#= println(b) =#
-#= Test.@test b == 0 =#
+Test.@test second(t0) == 8
+Test.@test second(t1) == 19208
+
+@time b = second(inp)
+println(b)
+Test.@test b == 12089663946752
