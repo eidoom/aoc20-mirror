d6f2d3061cc0cd33c70763e426ce7078683ec4e8
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-15 09:25:01 +0000

    15: optimisation and types for brute force

--- d15.jl
+++ d15.jl
@@ -4,18 +4,15 @@ import Test
 
 include("./com.jl")
 
-function read_file(name)
-    map(i -> parse(Int, i), split(Com.file_slurp(name), ","))
+function read_file(name::String)::Array{Int,1}
+    map(i::SubString -> parse(Int, i), split(Com.file_slurp(name), ","))
 end
 
-function game(spoken::Array{Int,1}, nth)
+function game(spoken::Array{Int,1}, nth::Int)::Int
     while length(spoken) < nth
-        prev = last(spoken)
-        if prev in spoken[1:(end - 1)]
-            push!(spoken, length(spoken) - findlast(i -> i == prev, spoken[1:(end - 1)]))
-        else
-            push!(spoken, 0)
-        end
+        index::Union{Int,Nothing} =
+            findlast(i::Int -> i === last(spoken), spoken[1:(end - 1)])
+        push!(spoken, index === nothing ? 0 : length(spoken) - index)
     end
     last(spoken)
 end
@@ -33,28 +30,28 @@ t6 = read_file("i15t6")
 
 inp = read_file("i15")
 
-Test.@test game(t0, one) == 436
-Test.@test game(t1, one) == 1
-Test.@test game(t2, one) == 10
-Test.@test game(t3, one) == 27
-Test.@test game(t4, one) == 78
-Test.@test game(t5, one) == 438
-Test.@test game(t6, one) == 1836
+Test.@test game(t0, one) === 436
+Test.@test game(t1, one) === 1
+Test.@test game(t2, one) === 10
+Test.@test game(t3, one) === 27
+Test.@test game(t4, one) === 78
+Test.@test game(t5, one) === 438
+Test.@test game(t6, one) === 1836
 
 @time a = game(inp, one)
 println(a)
-Test.@test a == 755
+Test.@test a === 755
 
 #= println(game(t0, two)) =#
 
-#= Test.@test game(t0, two) == 175594 =#
-#= Test.@test game(t1, two) == 2578 =#
-#= Test.@test game(t2, two) == 3544142 =#
-#= Test.@test game(t3, two) == 261214 =#
-#= Test.@test game(t4, two) == 6895259 =#
-#= Test.@test game(t5, two) == 18 =#
-#= Test.@test game(t6, two) == 362 =#
+#= Test.@test game(t0, two) === 175594 =#
+#= Test.@test game(t1, two) === 2578 =#
+#= Test.@test game(t2, two) === 3544142 =#
+#= Test.@test game(t3, two) === 261214 =#
+#= Test.@test game(t4, two) === 6895259 =#
+#= Test.@test game(t5, two) === 18 =#
+#= Test.@test game(t6, two) === 362 =#
 
 #= @time b = game(inp, two) =#
 #= println(b) =#
-#= Test.@test b == 0 =#
+#= Test.@test b === 0 =#
