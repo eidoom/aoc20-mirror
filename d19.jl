c998150ce337e88c2a233a1392d166c7bb134457
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-19 18:01:39 +0000

    19a

--- d19.jl
+++ d19.jl
@@ -6,15 +6,16 @@ include("./com.jl")
 
 function read_file(name)
     raw, mesgs = map(each -> split(each, '\n'), split(Com.file_slurp(name), "\n\n"))
-    rules = []
+    rules = Vector(undef, length(raw))
     for r in raw
         m = match(r"^(\d+): (.*)$", r)
+        ii = parse(Int, m.captures[1]) + 1
         r = m.captures[2]
         if r[1] == '"'
-            push!(rules, r[2])
+            rules[ii] = r[2]
         else
             r = map(i -> map(j -> parse(Int, j) + 1, split(i, " ")), split(r, " | "))
-            push!(rules, r)
+            rules[ii] = r
         end
     end
     rules, mesgs
@@ -27,29 +28,49 @@ function builder(rules, i)
         rule
     elseif length(rule) == 1
         rule = rule[1]
-        if length(rule) == 2
-            broadcast(*, builder(rules, rule[1]), builder(rules, rule[2]))
+        if length(rule) == 1
+            builder(rules, rule[1])
+        elseif length(rule) == 2
+            as = []
+            a1 = builder(rules, rule[1])
+            a2 = builder(rules, rule[2])
+            for i in a1
+                for j in a2
+                    push!(as, i * j)
+                end
+            end
+            as
         else
-            broadcast(
-                *,
-                builder(rules, rule[1]),
-                builder(rules, rule[2]),
-                builder(rules, rule[3]),
-            )
+            as = []
+            a1 = builder(rules, rule[1])
+            a2 = builder(rules, rule[2])
+            a3 = builder(rules, rule[3])
+            for i in a1
+                for j in a2
+                    for k in a3
+                        push!(as, i * j * k)
+                    end
+                end
+            end
+            as
         end
     else
         as = []
         for set in rule
-            p1, p2 = set
-            a1 = builder(rules, p1)
-            a2 = builder(rules, p2)
-            for i in a1
-                for j in a2
-                    push!(as, i*j)
+            if length(set) == 1
+                p0 = set[1]
+                a0 = builder(rules, p0)
+                push!(as, a0)
+            else
+                a1 = builder(rules, set[1])
+                a2 = builder(rules, set[2])
+                for i in a1
+                    for j in a2
+                        push!(as, i * j)
+                    end
                 end
             end
         end
-        println("as ", as)
         as
     end
 
@@ -57,44 +78,20 @@ end
 
 function one(data)
     rules, mesgs = data
-    #= println(rules) =#
     mesgs = Set(mesgs)
-    #= println(mesgs) =#
-    rules = Set(builder(rules, 1))
-    println("rules ", rules)
+    rules = builder(rules, 1)
     length(intersect(mesgs, rules))
-    #= as = [] =#
-    #= is = [1] =#
-    #= #1= while true =1# =#
-    #= i = pop!(is) =#
-    #= a = "" =#
-    #= rule = rules[i] =#
-
-    #= if isa(rule, Char) =#
-    #=     a *= rule =#
-    #= else =#
-    #=     for set in rule =#
-    #=         for pos in set =#
-    #=             push!(is, pos) =#
-    #=         end =#
-    #=     end =#
-    #= end =#
-    #= #1= end =1# =#
 end
 
-#= t0 = read_file("i19t0") =#
 t1 = read_file("i19t1")
-#= t2 = read_file("i19t2") =#
 inp = read_file("i19")
 
-#= println(one(t0)) =#
-@time println(one(t1))
-#= @time println(one(t2)) =#
-#= Test.@test one(t) == 0 =#
+println(one(t1))
+Test.@test one(t1) == 2
 
-#= @time a = one(inp) =#
-#= println(a) =#
-#= Test.@test a == 0 =#
+@time a = one(inp)
+println(a)
+Test.@test a == 224
 
 #= function two(data) =#
 #=     data =#
