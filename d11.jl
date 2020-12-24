09b04de45c3448fa6a262d4b710c4a3edbf448ab
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-24 15:53:50 +0000

    11: use Julia multi-dimensional arrays

--- d11.jl
+++ d11.jl
@@ -5,112 +5,111 @@ import Test
 include("./com.jl")
 
 function read_file(name)
-    collect.(Com.file_lines(name))
+    permutedims(hcat(collect.(Com.file_lines(name))...))
 end
 
 function seats1(data)
-    new = deepcopy(data)
-    h = length(new)
-    w = length(new[1])
+    new = copy(data)
+    h, w = size(new)
     c = 1
     new_c = 0
     while new_c != c
-        data = deepcopy(new)
+        data = copy(new)
         c = new_c
-        for i = 1:h
-            for j = 1:w
-                if data[i][j] === 'L'
+        for i in axes(new, 1)
+            for j in axes(new, 2)
+                if data[i, j] === 'L'
                     if i != 1
                         if j != 1
-                            if data[i - 1][j - 1] === '#'
+                            if data[i - 1, j - 1] === '#'
                                 continue
                             end
                         end
-                        if data[i - 1][j] === '#'
+                        if data[i - 1, j] === '#'
                             continue
                         end
                         if j != w
-                            if data[i - 1][j + 1] === '#'
+                            if data[i - 1, j + 1] === '#'
                                 continue
                             end
                         end
                     end
                     if j != 1
-                        if data[i][j - 1] === '#'
+                        if data[i, j - 1] === '#'
                             continue
                         end
                     end
                     if j != w
-                        if data[i][j + 1] === '#'
+                        if data[i, j + 1] === '#'
                             continue
                         end
                     end
                     if i != h
                         if j != 1
-                            if data[i + 1][j - 1] === '#'
+                            if data[i + 1, j - 1] === '#'
                                 continue
                             end
                         end
-                        if data[i + 1][j] === '#'
+                        if data[i + 1, j] === '#'
                             continue
                         end
                         if j != w
-                            if data[i + 1][j + 1] === '#'
+                            if data[i + 1, j + 1] === '#'
                                 continue
                             end
                         end
                     end
-                    new[i][j] = '#'
+                    new[i, j] = '#'
                 end
-                if data[i][j] === '#'
+                if data[i, j] === '#'
                     count = 0
                     if i != 1
                         if j != 1
-                            if data[i - 1][j - 1] === '#'
+                            if data[i - 1, j - 1] === '#'
                                 count += 1
                             end
                         end
-                        if data[i - 1][j] === '#'
+                        if data[i - 1, j] === '#'
                             count += 1
                         end
                         if j != w
-                            if data[i - 1][j + 1] === '#'
+                            if data[i - 1, j + 1] === '#'
                                 count += 1
                             end
                         end
                     end
                     if j != 1
-                        if data[i][j - 1] === '#'
+                        if data[i, j - 1] === '#'
                             count += 1
                         end
                     end
                     if j != w
-                        if data[i][j + 1] === '#'
+                        if data[i, j + 1] === '#'
                             count += 1
                         end
                     end
                     if i != h
                         if j != 1
-                            if data[i + 1][j - 1] === '#'
+                            if data[i + 1, j - 1] === '#'
                                 count += 1
                             end
                         end
-                        if data[i + 1][j] === '#'
+                        if data[i + 1, j] === '#'
                             count += 1
                         end
                         if j != w
-                            if data[i + 1][j + 1] === '#'
+                            if data[i + 1, j + 1] === '#'
                                 count += 1
                             end
                         end
                     end
                     if count >= 4
-                        new[i][j] = 'L'
+                        new[i, j] = 'L'
                     end
                 end
             end
         end
-        new_c = count(p -> p === '#', Iterators.flatten(new))
+        new_c = count(p -> p === '#', new)
     end
     c
 end
@@ -125,13 +124,13 @@ Test.@test seats1(t) === 37
 println(a)
 Test.@test a === 2126
 
-function ray(data, i, j, di, dj, w, h)
+function ray(data, i, j, di, dj, h, w)
     while true
         i += di
         j += dj
-        if !(1 <= i <= h && 1 <= j <= w) || data[i][j] === 'L'
+        if !(1 <= i <= h && 1 <= j <= w) || data[i, j] === 'L'
             return false
-        elseif data[i][j] === '#'
+        elseif data[i, j] === '#'
             return true
         end
     end
@@ -145,24 +144,23 @@ end
 #= end =#
 
 function seats2(d)
-    new = deepcopy(d)
-    h = length(new)
-    w = length(new[1])
+    new = copy(d)
+    h, w = size(new)
     c = 1
     new_c = 0
     dirs = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
     while new_c != c
-        data = deepcopy(new)
+        data = copy(new)
         c = new_c
-        for i = 1:h
-            for j = 1:w
-                if data[i][j] === 'L'
-                    if !any(dir -> ray(data, i, j, dir..., w, h), dirs)
-                        new[i][j] = '#'
+        for i in axes(new, 1)
+            for j in axes(new, 2)
+                if data[i, j] === 'L'
+                    if !any(dir -> ray(data, i, j, dir..., h, w), dirs)
+                        new[i, j] = '#'
                     end
-                elseif data[i][j] === '#'
-                    if count(dir -> ray(data, i, j, dir..., w, h), dirs) >= 5
-                        new[i][j] = 'L'
+                elseif data[i, j] === '#'
+                    if count(dir -> ray(data, i, j, dir..., h, w), dirs) >= 5
+                        new[i, j] = 'L'
                     end
                 end
             end
