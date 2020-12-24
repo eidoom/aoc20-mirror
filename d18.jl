d2dc0658c60028f1136d1046909f7b774ed6b562
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-24 21:52:35 +0000

    Neaten

--- d18.jl
+++ d18.jl
@@ -8,13 +8,14 @@ include("./com.jl")
 @enum Sym plus times left right
 
 const Letter = Union{Int,Sym}
+const Letters = Vector{Letter}
 
 #= turn strings into arrays of Int and Sym =#
-function read_file(name::String)::Vector{Vector{Letter}}
-    lines = Vector{Letter}[]
-    for expr::String in Com.file_lines(name)
+function read_file(name::String)::Vector{Letters}
+    lines = Letters[]
+    for expr in Com.file_lines(name)
         line = Letter[]
-        for i::SubString in split(expr)
+        for i in split(expr)
             if i[1] === '+'
                 push!(line, plus)
             elseif i[1] === '*'
@@ -42,21 +43,20 @@ end
 
 #= pure left-to-right operator precedence =#
 #= it's faster to return an array here than an Int =#
-function arithmetic(line::Vector{Letter})::Vector{Letter}
+function arithmetic(line::Letters)::Letters
     while length(line) > 1
-        line::Vector{Letter} =
-            vcat([line[2] === plus ? line[1] + line[3] : line[1] * line[3]], line[4:end])
+        line = vcat([line[2] === plus ? line[1] + line[3] : line[1] * line[3]], line[4:end])
     end
     line
 end
 
 #= "recursive descent parser" for parentheses =#
-function brackets(line::Vector{Letter}, maths)::Vector{Letter}
-    check = findfirst(i::Letter -> i === left, line)
+function brackets(line::Letters, maths::Function)::Letters
+    check = findfirst(i -> i === left, line)
     if check !== nothing
         brackets(vcat(line[1:(check - 1)], brackets(line[(check + 1):end], maths)), maths)
     else
-        finish = findfirst(i::Letter -> i === right, line[1:end])
+        finish = findfirst(i -> i === right, line[1:end])
         if finish !== nothing
             vcat(maths(line[1:(finish - 1)]), line[(finish + 1):end])
         else
@@ -66,7 +66,7 @@ function brackets(line::Vector{Letter}, maths)::Vector{Letter}
 end
 
 #= sum of result of each expression =#
-function evaluate(data::Vector{Vector{Letter}}, maths)::Int
+function evaluate(data::Vector{Letters}, maths::Function)::Int
     sum(line -> brackets(line, maths)[1], data)
 end
 
@@ -81,14 +81,13 @@ Test.@test a === 280014646144
 
 #= addition has higher operation precedence than multiplication =#
 #= it would be faster to return an Int here, but then part 1 would be slowed down more than the gain in part 2 =#
-function arithmetic2(line::Vector{Letter})::Vector{Letter}
+function arithmetic2(line::Letters)::Letters
     while length(line) > 1
-        i = findfirst(c::Letter -> c === plus, line)
+        i = findfirst(c -> c === plus, line)
         if i === nothing
             return [reduce(*, line[1:2:end])]
         end
-        line::Vector{Letter} =
-            vcat(line[1:(i - 2)], [line[i - 1] + line[i + 1]], line[(i + 2):end])
+        line = vcat(line[1:(i - 2)], [line[i - 1] + line[i + 1]], line[(i + 2):end])
     end
     line
 end
