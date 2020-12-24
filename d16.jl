1afde0d9d805cc02e2d4cbcf540574bd9e659ed6
Ryan Moodie <ryanmoodie@gmail.com>
2020-12-24 20:04:19 +0000

    16: types

--- d16.jl
+++ d16.jl
@@ -4,39 +4,42 @@ import Test
 
 include("./com.jl")
 
-function read_rules(data)
-    rules = Dict()
+const Rule = NTuple{2,NTuple{2,Int}}
+const Rules = Dict{String,Rule}
+const Ticket = Vector{Int}
+const Tickets = Vector{Ticket}
+
+function read_rules(data::Vector{SubString{String}})::Rules
+    rules = Rules()
     for d in data
         m = match(r"^([\w ]+): (\d+)-(\d+) or (\d+)-(\d+)$", d)
         if m !== nothing
-            rules[m.captures[1]] = map(
-                j -> parse.(Int, map(k -> m.captures[j + k], (0, 1))),
-                (2, 4),
-            )
+            rules[m.captures[1]] =
+                map(j -> parse.(Int, map(k -> m.captures[j + k], (0, 1))), (2, 4))
         end
     end
     rules
 end
 
-function read_tickets(data)
+function read_tickets(data::Vector{SubString{String}})::Tickets
     map(line -> parse.(Int, split(line, ",")), data[2:end])
 end
 
-function read_file(name)
-    rules, yours, nearby = map(sec -> split(sec, '\n'), Com.file_paras(name))
+function read_file(name::String)::Tuple{Rules,Ticket,Tickets}
+    rules, yours, nearby = split.(Com.file_paras(name), '\n')
     read_rules(rules), first(read_tickets(yours)), read_tickets(nearby)
 end
 
-function check_ticket(rules, n)
-    any((low <= n <= high) for (low, high) in rules)
+function check_ticket(rule::Rule, n::Int)
+    any((low <= n <= high) for (low, high) in rule)
 end
 
-function check_valid(rules, n)
+function check_valid(rules::Rules, n::Int)
     any(rule -> check_ticket(rule, n), values(rules))
 end
 
 #= check each number of each ticket for validity by any field =#
-function one(data)
+function one(data::Tuple{Rules,Ticket,Tickets})::Int
     rules, _, nearby = data
     invalid = 0
     for ticket in nearby
@@ -50,18 +53,18 @@ function one(data)
 end
 
 t0 = read_file("i16t0")
-inp = read_file("i16")
+@time inp = read_file("i16")
 
-Test.@test one(t0) == 71
+Test.@test one(t0) === 71
 
 @time a = one(inp)
 println(a)
-Test.@test a == 27911
+Test.@test a === 27911
 
 #= go through "columns of the ticket matrix" to see which fields they satisfy =#
-function possibilities(rules, tickets)
+function possibilities(rules::Rules, tickets::Tickets)::Vector{Vector{String}}
     pos = []
-    for col = 1:length(tickets[1])
+    for col in axes(tickets[1], 1)
         this = []
         for (name, ranges) in rules
             if all(ticket -> check_ticket(ranges, ticket[col]), tickets)
@@ -74,30 +77,37 @@ function possibilities(rules, tickets)
 end
 
 #= find the correct choice of field for each column by iteratively setting columns with only one choice and removing that choice from its positions (so then there's another with only one choice, and so on) =#
-function ordering(pos)
-    order = Array{String}(undef, length(pos))
+function ordering(pos::Vector{Vector{String}})::Vector{String}
+    order = Vector{String}(undef, length(pos))
     i = 1
-    while any(p -> length(p) != 0, pos)
-        if length(pos[i]) == 1
+    while any(p -> length(p) !== 0, pos)
+        if length(pos[i]) === 1
             cur = pop!(pos[i])
             order[i] = cur
             for p in pos
-                filter!(i -> i != cur, p)
+                filter!(j -> j !== cur, p)
             end
         end
-        i = mod(i + 1, 1:length(pos))
+        i = mod(i + 1, axes(pos, 1))
     end
     order
 end
 
 #= look at tickets which contain only numbers that are valid by any field, then find the correct ordering of fields, then give product of departure fields =#
-function two(data)
+function two(
+    data::Tuple{Rules,Ticket,Tickets},
+)::Iterators.Zip{Tuple{Vector{String},Vector{Int}}}
     rules, yours, nearby = data
     valid = filter(ticket -> all(n -> check_valid(rules, n), ticket), nearby)
     pos = possibilities(rules, valid)
     unique = ordering(pos)
+    zip(unique, yours)
+end
+
+function departures(data::Tuple{Rules,Ticket,Tickets})::Int
+    sorted = two(data)
     res = 1
-    for (field, num) in zip(unique, yours)
+    for (field, num) in sorted
         if contains(field, "departure ")
             res *= num
         end
@@ -105,9 +115,9 @@ function two(data)
     res
 end
 
-#= t1 = read_file("i16t1") =#
-#= @time two(t1) =#
+t1 = read_file("i16t1")
+Test.@test collect(two(t1)) == [("row", 11), ("class", 12), ("seat", 13)]
 
-@time b = two(inp)
+@time b = departures(inp)
 println(b)
-Test.@test b == 737176602479
+Test.@test b === 737176602479
